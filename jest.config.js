d784cd1a8585a9736840c52c38f43a43e3621b96
LuoHao <hluo5206@gmail.com>
2022-03-05 11:35:10 +0800

    docs(cache): cache

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  rootDir: '.',
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+  transform: {
+    "\\.js$": 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    "/node_modules/",
+  ],
+  watchman: false,
+};
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
+
+function resolve(params) {
+  return path.resolve(__dirname, params);
+}
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: resolve("src/index.js")
+  },
+  output: {
+    filename: "[name].js",
+    path: resolve("dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.(css|less)$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.(png|jpg|svg)$/,
+        loader: "file-loader",
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "@": resolve("src"),
+      "vue": "vue/dist/vue.js"
+    },
+    extensions: [".js"],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: resolve("./public/index.html"),
+      favicon: resolve("./public/favicon.ico"),
+    })
+  ],
+  devServer: {
+    port: 8099
+  },
+};
